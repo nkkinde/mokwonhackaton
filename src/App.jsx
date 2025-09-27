9b6c77c0c48aa24908d3c2ab8a42bcc1ecfcb0d1
Hyeon <dolphin@kakao.com>
2025-09-27 21:52:27 +0900

    add test page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
 import MainPage from './DashBoardPage.jsx';
+import TestPage from './Pages/TestPage.jsx';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/MainPage" element={<MainPage />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/test" element={<TestPage />} />
         </Routes>
     );
 }
