a1a792e61c3dd159daa44aea3ec951639f7fa8c5
Juhyoungd <Juhyoungd@gmail.com>
2025-09-28 00:06:40 +0900

    새거
    메인화면

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import Login from './Login.jsx';
-import Signup from './Signup.jsx';
-import MainPage from './DashBoardPage.jsx';
+import Login from './Pages/Login.jsx';
+import Signup from './Pages/Signup.jsx';
+import MainPage from './Pages/DashBoardPage.jsx';
 import TestPage from './Pages/TestPage.jsx';
-import './App.css';
+import DashBoardPage from './Pages/DashBoardPage.jsx';
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<DashBoardPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/MainPage" element={<MainPage />} />
             <Route path="/signup" element={<Signup />} />
